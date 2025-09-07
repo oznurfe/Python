a5af02c143e10a07440f6dd83f47f1d333cf15a9
oznurfe <oznurdemr19@gmail.com>
2025-09-07 15:14:01 +0300

    Secret Auction.py

--- Secret Auction.py
+++ Secret Auction.py
@@ -1,6 +1,6 @@
-import art
+import art1
 answer = "yes"
-print(art.logo)
+print(art1.logo)
 print("Welcome to the secret auction program.")
 Secret_Auction = {}
 while answer == "yes":
@@ -19,4 +19,5 @@ for key in Secret_Auction:
         winner_name = key
 
 print(f"The winner is {winner_name} with a bid ${winner}")
+
 
