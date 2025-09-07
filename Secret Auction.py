37fd82f6f72e23b6410016303dc47bbbbc06dc09
oznurfe <oznurdemr19@gmail.com>
2025-09-07 13:26:45 +0300

    Add files via upload

---
+++ Secret Auction.py
@@ -0,0 +1,22 @@
+import art
+answer = "yes"
+print(art.logo)
+print("Welcome to the secret auction program.")
+Secret_Auction = {}
+while answer == "yes":
+    name = input("What is your name? ")
+    bid = int(input("What's your bid? $"))
+
+    Secret_Auction[name] = bid
+    answer = input("Are they any other bidders? Type 'yes' or 'no': ")
+    print("\n" * 100)
+print("\n" * 100)
+winner = 0
+winner_name = ""
+for key in Secret_Auction:
+    if winner < Secret_Auction[key]:
+        winner = Secret_Auction[key]
+        winner_name = key
+
+print(f"The winner is {winner_name} with a bid ${winner}")
+
